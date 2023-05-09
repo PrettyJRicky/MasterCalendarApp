2ceff18f8ed700eb338d637a1c1dc597ffcce0ba
PrettyJRicky <patrickjayestrada@yahoo.com>
2023-05-09 18:47:54 +0200

    Charlie Version

---
+++ app/src/main/java/com/example/mastercalendarapp/shared/ContinuousSelectionHelper.kt
@@ -0,0 +1,65 @@
+package com.example.mastercalendarapp.shared
+
+import com.example.mastercalendarapp.core.atStartOfMonth
+import com.example.mastercalendarapp.core.nextMonth
+import com.example.mastercalendarapp.core.previousMonth
+import com.example.mastercalendarapp.core.yearMonth
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
+import kotlin.LazyThreadSafetyMode.NONE
+
+data class DateSelection(val startDate: LocalDate? = null, val endDate: LocalDate? = null) {
+    val daysBetween by lazy(NONE) {
+        if (startDate == null || endDate == null) null else {
+            ChronoUnit.DAYS.between(startDate, endDate)
+        }
+    }
+}
+
+private val rangeFormatter = DateTimeFormatter.ofPattern("d MMMM yyyy")
+fun dateRangeDisplayText(startDate: LocalDate, endDate: LocalDate): String {
+    return "Selected: ${rangeFormatter.format(startDate)} - ${rangeFormatter.format(endDate)}"
+}
+
+object ContinuousSelectionHelper {
+    fun getSelection(
+        clickedDate: LocalDate,
+        dateSelection: DateSelection,
+    ): DateSelection {
+        val (selectionStartDate, selectionEndDate) = dateSelection
+        return if (selectionStartDate != null) {
+            if (clickedDate < selectionStartDate || selectionEndDate != null) {
+                DateSelection(startDate = clickedDate, endDate = null)
+            } else if (clickedDate != selectionStartDate) {
+                DateSelection(startDate = selectionStartDate, endDate = clickedDate)
+            } else {
+                DateSelection(startDate = clickedDate, endDate = null)
+            }
+        } else {
+            DateSelection(startDate = clickedDate, endDate = null)
+        }
+    }
+
+    fun isInDateBetweenSelection(
+        inDate: LocalDate,
+        startDate: LocalDate,
+        endDate: LocalDate,
+    ): Boolean {
+        if (startDate.yearMonth == endDate.yearMonth) return false
+        if (inDate.yearMonth == startDate.yearMonth) return true
+        val firstDateInThisMonth = inDate.yearMonth.nextMonth.atStartOfMonth()
+        return firstDateInThisMonth in startDate..endDate && startDate != firstDateInThisMonth
+    }
+
+    fun isOutDateBetweenSelection(
+        outDate: LocalDate,
+        startDate: LocalDate,
+        endDate: LocalDate,
+    ): Boolean {
+        if (startDate.yearMonth == endDate.yearMonth) return false
+        if (outDate.yearMonth == endDate.yearMonth) return true
+        val lastDateInThisMonth = outDate.yearMonth.previousMonth.atEndOfMonth()
+        return lastDateInThisMonth in startDate..endDate && endDate != lastDateInThisMonth
+    }
+}
